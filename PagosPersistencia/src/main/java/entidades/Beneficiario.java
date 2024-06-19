717ee67cf3c31bdf1fd7bf86a21ff7a26ddf3781
JesusLares23 <jesus.lares233383@potros.itson.edu.mx>
2024-06-19 06:36:57 -0700

    Adición de entidad Beneficiario

--- PagosPersistencia/src/main/java/entidades/Beneficiario.java
+++ PagosPersistencia/src/main/java/entidades/Beneficiario.java
@@ -1,58 +1,44 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package entidades;
 
 import java.io.Serializable;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 /**
  *
  * @author darkm
  */
 @Entity
+@Table(name = "Beneficiarios")
 public class Beneficiario implements Serializable {
 
     private static final long serialVersionUID = 1L;
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id_beneficiario")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (id != null ? id.hashCode() : 0);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof Beneficiario)) {
-            return false;
-        }
-        Beneficiario other = (Beneficiario) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return "entidades.Beneficiario[ id=" + id + " ]";
-    }
+    
+    @Column(name = "clave_contrato", nullable = false)
+    private int claveContrato;
+    
+    @Column(name = "nombres", length = 50, nullable = false)
+    private String nombres;
+    
+    @Column(name = "apellido_paterno", length = 50, nullable = false)
+    private String apellidoPaterno;
+    
+    @Column(name = "apellido_materno", length = 50, nullable = true)
+    private String apellidoMaterno;
+    
+    @Column(name = "usuario", length = 50, nullable = false)
+    private String usuario;
+    
+    @Column(name = "contra", length = 100, nullable = false)
+    private String contra
     
 }
